40e0bd22f93cc4f6c2966d8b4c83f09280780dfa
Ismile Hossain <iamismile.dev@gmail.com>
2023-09-16 18:37:14 +0600

    dark & light theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from '@/components/mode-toogle';
 import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
