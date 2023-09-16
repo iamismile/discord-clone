7ce376cc4d897e84335bef1c9e6a67d8befe82d9
Ismile Hossain <iamismile.dev@gmail.com>
2023-09-16 18:24:17 +0600

    authentication

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-  return (
-    <div>
-      <p>Login page</p>
-    </div>
-  );
-};
-
-export default LoginPage;
--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-  return (
-    <div>
-      <p>Register page</p>
-    </div>
-  );
-};
-
-export default RegisterPage;
