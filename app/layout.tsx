9461a1bc40ff2780d4e562946f13ec7e1a5ed0e8
Ismile Hossain <iamismile.dev@gmail.com>
2023-10-07 14:03:30 +0600

    create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import { ModalProvider } from '@/components/providers/modal-provider';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
-import { ThemeProvider } from '@/components/providers/theme-provider';
-import { cn } from '@/lib/utils';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
